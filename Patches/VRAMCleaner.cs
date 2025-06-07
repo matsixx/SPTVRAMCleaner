24d81b465ee242bf027587c404d3ad3f605245d9
matsix <lilmatsix@gmail.com>
2025-06-07 14:23:42 -0400

    v1.0

---
+++ Patches/VRAMCleaner.cs
@@ -0,0 +1,73 @@
+﻿using EFT;
+using EFT.UI.Matchmaker;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SPTVRAMCleaner.Patches
+{
+    internal class VRAMCleaner : ModulePatch // all patches must inherit ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            // one way methods can be patched is by targeting both their class name and the name of the method itself
+            // the example in this patch is the Jump() method in the Player class
+            return AccessTools.Method(typeof(Player), nameof(Player.VisualPass));
+        }
+        
+        private static bool _hasCountdownFinished = false;
+        private static float _countdownFinishedTime = -1f;
+        private const float DelaySeconds = 3f;
+
+        [PatchPostfix]
+        static void Postfix(Player __instance)
+        {
+            if (!__instance.IsYourPlayer)
+                return;
+
+            if (_hasCountdownFinished && Time.realtimeSinceStartup >= _countdownFinishedTime + DelaySeconds)
+            {
+                _hasCountdownFinished = false; // reset or prevent re-running
+                _countdownFinishedTime = -1f;
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                Resources.UnloadUnusedAssets();
+
+                Plugin.MyLog.LogWarning("Unused Assets have been unloaded - VRAM Usage cleaned");
+            }
+        }
+        public static void SetCountdownFinished()
+        {
+            _hasCountdownFinished = true;
+            _countdownFinishedTime = Time.realtimeSinceStartup;
+        }
+    }
+
+    internal class VRAMCleanerInit : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(MatchmakerFinalCountdown), nameof(MatchmakerFinalCountdown.Update));
+        }
+
+        private static readonly AccessTools.FieldRef<MatchmakerFinalCountdown, DateTime> _dateTimeRef =
+    AccessTools.FieldRefAccess<MatchmakerFinalCountdown, DateTime>("dateTime_0");
+
+        [PatchPostfix]
+        private static void Postfix(MatchmakerFinalCountdown __instance)
+        {
+            TimeSpan timeSpan = _dateTimeRef(__instance) - EFTDateTimeClass.Now;
+            if (timeSpan.TotalSeconds <= 0.0)
+            {
+                VRAMCleaner.SetCountdownFinished();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+SPTVRAMCleaner
+
+================
+
+Have you ever wanted to play SPT at 12k resolution but your VRAM keeps maxing out?
+
+Well, now you can! No not really, but this mod should help clear up about 1-2GB of VRAM. There will be a small lag spike about 3 seconds after the raid countdown finishes.
